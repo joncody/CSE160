ca2ed403aeb2bf07aefbc0136efc9087362f14e9
Jon Cody <joncodyyy@gmail.com>
2026-04-03 18:38:30 -0700

    using equilateral triangles now

--- asgn1/src/Triangle.js
+++ asgn1/src/Triangle.js
@@ -11,18 +11,24 @@ class Triangle {
     render() {
         gl.uniform4f(u_FragColor, this.color[0], this.color[1], this.color[2], this.color[3]);
         gl.uniform1f(u_Size, this.size);
-
         if (this.vertices === null) {
+            let [x, y] = this.position;
             let d = this.size / 200.0;
+
+            // Equilateral Triangle Math:
+            // Height = side * sqrt(3)/2 ≈ side * 0.866
+            let h = d * (Math.sqrt(3) / 2);
+
+            // Center the triangle on the mouse click
             this.vertices = new Float32Array([
-                this.position[0], this.position[1],
-                this.position[0] + d, this.position[1],
-                this.position[0], this.position[1] + d
+                x - d/2, y - h/3,   // Bottom Left
+                x + d/2, y - h/3,   // Bottom Right
+                x,       y + 2*h/3  // Top Tip
             ]);
         }
-
-        if (this.buffer === null) this.buffer = gl.createBuffer();
-
+        if (this.buffer === null) {
+            this.buffer = gl.createBuffer();
+        }
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
         gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.DYNAMIC_DRAW);
         gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
